8b31f24cb4772b7673e557e2cf4ce925c552b93c
Joe Houghes <joehoughes@gmail.com>
2022-12-19 08:42:16 -0700

    Convery New-cVBRSobr.ps1 and New-cVB365OrgRepos.ps1 to advanced functions

--- New-cVBRSobr.ps1
+++ New-cVBRSobr.ps1
@@ -1,54 +1,85 @@
 <#
 .Synopsis
 For Veeam Backup & Replication v12 this script will create the desired number of buckets with a given prefix and then add them into a SOBR
-Requires aws cli to be installed and configured with a profile via "aws configure --profile profilename" to create buckets
 
 .Notes
 Version: 1.0
-Author: Jim Jones, @k00laidIT
-Modified Date: 12/16/2022
+Authors: Jim Jones, @k00laidIT; Joe Houghes @jhoughes
+Modified Date: 12/19/2022
 
 .EXAMPLE
-.\New-cVbrSobr.ps1
+./New-cVBRSOBR.ps1  #to local function into memory
+New-cVBRSOBR -NamePrefix '123-test1' -VBRSrv 'localhost' -SvcPoint 'https://us-central-1a.object.ilandcloud.com' -AWSProfile 'ilanduscentral' -NumRepos '5' -IMM $true -IMMDays '30'
 #>
 
-$nameprefix = "123-test1"
-$vbrsrv = "localhost"
-$svcpoint = "https://us-central-1a.object.ilandcloud.com" 
-$awsprofile = "ilanduscentral"
-$numrepos = "5"
-$imm = $true
-$immdays = "30"
 
-Connect-VBRServer -Server $vbrsrv
+function New-cVBRSOBR {
 
-$i = 1
-do {
-    #Create bucket via aws cli    
-    $bucketname = $nameprefix+"-"+$i
-    if ($imm -eq $true) {
-        Invoke-Command -ScriptBlock {aws --endpoint $svcpoint --profile $awsprofile --no-verify-ssl s3api create-bucket --object-lock-enabled-for-bucket --bucket $bucketname}
-    }
-    else {
-        Invoke-Command -ScriptBlock {aws --endpoint $svcpoint --profile $awsprofile --no-verify-ssl s3api create-bucket --bucket $bucketname}
-    }
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $NamePrefix,
 
-    #Create object storage repository in VBR v12
-    $objkey = Get-VBRAmazonAccount -Id 02be1fe1-de9e-4bbf-a76b-2ab90d88fe1e
-    $connect = Connect-VBRAmazonS3CompatibleService -Account $objkey -CustomRegionId "us-east-1" -ServicePoint $svcpoint -Force
-    $bucket = Get-VBRAmazonS3Bucket -Connection $connect -Name $bucketname
-    $folder = New-VBRAmazonS3Folder -Bucket $bucket -Connection $connect -Name "Veeam" 
-    if ($imm -eq $true) {
+    [Parameter(Mandatory = $true)]
+    [string] $VBRSrv,
+
+    [Parameter(Mandatory = $true)]
+    [string] $SvcPoint,
+
+    [Parameter(Mandatory = $true)]
+    [string] $AWSProfile,
+
+    [Parameter(Mandatory = $true)]
+    [int] $NumRepos,
+
+    [Parameter(Mandatory = $true)]
+    [boolean] $IMM,
+
+    [Parameter(Mandatory = $true)]
+    [int] $IMMDays
+
+  )
+
+  begin {
+    #import-module AWS.Tools.Common, AWS.Tools.S3
+    Connect-VBRServer -Server $VBRSrv
+
+  } #end begin block
+
+  process {
+    $i = 1
+    do {
+      #Create bucket via aws cli
+      $bucketname = $nameprefix + '-' + $i
+      if ($IMM) {
+        Invoke-Command -ScriptBlock { aws --endpoint $svcpoint --profile $awsprofile --no-verify-ssl s3api create-bucket --object-lock-enabled-for-bucket --bucket $bucketname }
+      } else {
+        Invoke-Command -ScriptBlock { aws --endpoint $svcpoint --profile $awsprofile --no-verify-ssl s3api create-bucket --bucket $bucketname }
+      }
+
+      #Create object storage repository in VBR v12
+      $objkey = Get-VBRAmazonAccount -Id 02be1fe1-de9e-4bbf-a76b-2ab90d88fe1e
+      $connect = Connect-VBRAmazonS3CompatibleService -Account $objkey -CustomRegionId 'us-east-1' -ServicePoint $SvcPoint -Force
+      $bucket = Get-VBRAmazonS3Bucket -Connection $connect -Name $bucketname
+      $folder = New-VBRAmazonS3Folder -Bucket $bucket -Connection $connect -Name 'Veeam'
+      if ($IMM) {
         Add-VBRAmazonS3CompatibleRepository -Connection $connect -AmazonS3Folder $folder -Name $bucketname -EnableBackupImmutability -ImmutabilityPeriod $immdays
-    }
-    else {
+      } else {
         Add-VBRAmazonS3CompatibleRepository -Connection $connect -AmazonS3Folder $folder -Name $bucketname
-    }    
+      }
 
-    $i++
-} while ($i -le $numrepos)
+      $i++
+    } while ($i -le $NumRepos)
 
-#Put all the created repos in a SOBR
-$objrepo = Get-VBRBackupRepository | where {$_.Name -like "$nameprefix*"}
-$sobrname = $nameprefix+"-sobr"
-Add-VBRScaleOutBackupRepository -Name $sobrname -Extent $objrepo -PolicyType "DataLocality"
+  } #end process block
+
+  end {
+
+    #Put all the created repos in a SOBR
+    $objrepo = Get-VBRBackupRepository | Where-Object { $_.Name -like "$NamePrefix*" }
+    $sobrname = $NamePrefix + '-sobr'
+    Add-VBRScaleOutBackupRepository -Name $sobrname -Extent $objrepo -PolicyType 'DataLocality'
+
+  } #end end block
+
+} #end function
