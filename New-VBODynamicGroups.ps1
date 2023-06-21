a7207b31b3d299ce18fde8c4be2eb201c68a85c8
Jim Jones <jjones@iland.com>
2023-06-21 10:45:38 -0400

    added install command for the module

--- New-VBODynamicGroups.ps1
+++ New-VBODynamicGroups.ps1
@@ -3,6 +3,9 @@
 Script utilizes a specified CSV file to supply group names, RegEx expressions and usage locations to create dynamic groups 
 in Azure AD for use by Veeam Backup for Microsoft365. Requires Azure AD Premium P1 or greater subscription.
 Also requires the AzureADPreview module to be available requiring it to be run from Amd64 architecture
+
+Install-Module AzureADPreview -Scope CurrentUser -Force
+
 .Notes
 Version: 1.0
 Authors: Jim Jones, @k00laidIT
@@ -11,6 +14,7 @@ Modified Date: 06/21/2023
 .EXAMPLE
 .\New-VBODynamicGroups.ps1
 #>
+Import-Module AzureADPreview
 
 $SourceCsv = ".\sourcecsv.csv"
 
